1b8678ed13a259442b96afb0d80742588c11bfc7
Miky.Gabry <isgromichael@gmail.com>
2023-11-13 14:27:39 -0500

    Add immages link at views courses database

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('courses', views.CourseList.as_view(), name="course_list"),
+    path('courses/', views.CourseList.as_view(), name="course_list"),
     path('about/OurMission', views.About_OurMission.as_view(), name="About_OurMission"),
     path('about/EducationalPods', views.About_EducationalPods.as_view(), name="About_EducationalPods"),
     path('about/Schools', views.About_Schools.as_view(), name="About_Schools"),
